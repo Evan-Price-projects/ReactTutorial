3ef7296a52724d015fe29d1547d2fb75bb26b4d0
Evan <evan.price@flippos.com>
2021-06-13 13:51:47 -0600

    ADD: got it working with mongo

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,6 @@ const withDB = async (operations) => {
     try {
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
         const db = client.db('my-site');
-        console.log('a')
         await operations(db);
 
         client.close();
@@ -23,11 +22,9 @@ app.use(express.json())
 
 app.get('/api/articles/:id', async (req, res) => {
     withDB(async (db) => {
-        console.log('asdfk\n\n\n\n\n\n')
         const art = req.params.id;
 
-        const article = await db.collection('articles').findOne({ id: Number(art) })
-
+        const article = await db.collection('articles').find({ group: Number(art) }).toArray()
         res.status(200).json(article);
     },res)
 })
@@ -37,7 +34,7 @@ app.post('/api/articles/:id/update-title', async (req, res) => {
     console.log('title',  title)
     withDB(async (db) => {
         const art = req.params.id;
-        const article = await db.collection('articles').findOne({ id: Number(art) })
+        const article = await db.collection('articles').findOne({ group: Number(art) })
     
         await db.collection('articles').updateOne({
             id: Number(art)
@@ -46,16 +43,11 @@ app.post('/api/articles/:id/update-title', async (req, res) => {
                 title: title
             }
         });
-        const updatedArticleInfo = await db.collection('articles').findOne({ id: Number(art) })
+        const updatedArticleInfo = await db.collection('articles').findOne({ group: Number(art) })
         res.status(200).json(updatedArticleInfo)
     },res)
 })
 
-/* app.post('/api/articles/:id/add', (req, res) => {
-    const art = req.params.id;
-    let arty = articles.find(art1 => art1.id === Number(art))
-    res.status(200).send(`${arty}`)
-}) */
 
 app.get('/hello', (req, res) => res.send('Hello!'));
 app.post('/hello', (req, res) => res.send(`Hey! ${req.body.name}`));
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,37 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ModalBody } from 'react-bootstrap';
 import CardMaker from '../helpers/CardMaker';
-import articles from '../img/articles';
 
 const HomePage = (props: { match: { params: { id: string } } }) => {
+
     let { params } = props.match;
-    const card = articles.filter(ar => ar.group === Number(params.id));
-    if (card.length>0) {
-        let matcher =                
-        card.map((indCard, key)=>(
-            <CardMaker key={indCard.group}
-            title={indCard.title}
-            subTitle={indCard.subTitle}
-            content={indCard.content}
-        />
-        )  )
+
+    const [card, setArticleInfo] = useState([{content: '', id: 0, title: '', subTitle: '', group: 0 }]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${params.id}`)
+            const body = await result.json();
+            setArticleInfo(body)
+        }
+        fetchData()
+    }, [params.id])
+    console.log('card', card)
+    if (card.length > 0) {
+        let matcher =
+            card.map((indCard, key) => (
+                <CardMaker key={indCard.id}
+                    title={indCard.title}
+                    subTitle={indCard.subTitle}
+                    content={indCard.content}
+                />
+            ))
         return (
             <div className="card-padding">
                 {matcher}
             </div>
         )
-    } else if( params.id === undefined) {
+    } else if (params.id === undefined) {
         return (
             <div className="card-padding">
-                    <CardMaker
-                        title="Homepage"
-                        subTitle="Welcome to my website"
-                        content="This website was created to have fun with react"
-                    />
+                <CardMaker
+                    title="Homepage"
+                    subTitle="Welcome to my website"
+                    content="This website was created to have fun with react"
+                />
             </div>
         )
     }
-    else{
-        return(
+    else {
+        return (
             <></>
         )
     }
